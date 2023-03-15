6033c6fb8f0d60a23d2edc6f7dbfafaa5aaedba7
JulienDauphin <juliendauphin.openclassrooms@gmail.com>
2023-03-15 15:57:11 +0100

    création des fichiers de base pour mon thème du projet 11

---
+++ page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template pour les pages
+ **/
+
+get_header();
+
+/* Lancement de la boucle */
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'template-parts/content/content-page' );
+
+	// Si les commentaires sont autorisés et qu'il y a au moins un commentaire alors on charge le modèle de commentaire
+	if ( comments_open() || get_comments_number() ) {
+		comments_template();
+	}
+endwhile; // Fin de la boucle
+
+get_footer();
---
+++ readme.txt
@@ -0,0 +1,14 @@
+Thème : DauphinJulien
+Créé par : Dauphin Julien
+
+Description
+Thème de base réalisé pour le projet 11 de ma formation Wordpress chez Openclassrooms
+
+Version 1.0 
+Realisé le 15 Mars 2023
+
+https://juliendauphin.fr
+
+Copyright
+
+DauphinJulien, (C) 2023
