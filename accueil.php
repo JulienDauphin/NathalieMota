ae9936923d38c63cc4c6346b751b176c3f83536b
JulienDauphin <juliendauphin.openclassrooms@gmail.com>
2023-03-21 15:13:38 +0100

    ajout hero en background

--- accueil.php
+++ accueil.php
@@ -1,9 +1,17 @@
-
+<?php
+/*
+  Template Name: Accueil
+*/
+get_header(); ?>
+	
+<div class="hero">
+  
 <?php
       // On place les critères de la requête dans un Array
         $args = array(
           'post_type' => 'photo',
-          'posts_per_page' => 8,
+          'posts_per_page' => 1,
+          'orderby' => 'rand',
           
         );
         //On crée ensuite une instance de requête WP_Query basée sur les critères placés dans la variables $args
@@ -11,15 +19,16 @@
         ?>
         <!-- //On vérifie si le résultat de la requête contient des articles -->
         <?php if ($query->have_posts()): ?>
-          <div class="container_thumbnail_block">
+          <div class="container_thumbnail_accueil">
             <!-- //On parcourt chacun des articles résultant de la requête -->
             <?php while ($query->have_posts()): ?>
               <?php $query->the_post(); ?>
-              <div class="news_block">
+              <div class="news_accueil">
                 <?php if (has_post_thumbnail()): ?>
-                  <div class="thumbnail-block">
-                  <a href="<?php the_permalink(); ?>">
-                    <?php the_post_thumbnail(array(550, 550)); ?>
+                  <div class="thumbnail-accueil">
+                    <a href="<?php the_permalink(); ?>">
+                    <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+echo '<div class="produkt" style="background: url('. $url.'); background-size: cover">'; ?>
                     </a>
                   </div>
                 <?php endif; ?>
@@ -31,3 +40,14 @@
         <?php endif;
         wp_reset_query();
         ?>
+</div>
+
+
+
+    <div class="content">
+    <?php the_content(); ?>
+    </div> 
+    <?php 
+// Le haut de l'interface est ajouté avant le contenu
+include 'templates_parts/photo_block.php'; ?>
+	<?php get_footer(); ?>
--- page.php
+++ page.php
@@ -5,16 +5,7 @@
 
 get_header();
 
-/* Lancement de la boucle */
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
-
-	// Si les commentaires sont autorisés et qu'il y a au moins un commentaire alors on charge le modèle de commentaire
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-endwhile; // Fin de la boucle
+the_content();
 
 
 get_footer();
